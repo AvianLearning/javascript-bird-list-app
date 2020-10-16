43d37df52fcd2346157a4164dce6b1d819cacd4b
AvianLearning <iaradcliffe@hotmail.com>
2020-10-16 19:09:07 +0100

    Populated HTML page

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newItemForm = document.querySelector('#new-bird-form');
+    newItemForm.addEventListener('submit', handleNewItemFormSubmit);
+
+})
+
+const handleNewItemFormSubmit = function (event) {
+    event.preventDefault();
+
+    
+}
