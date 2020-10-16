b69b15bb57928e84f6e0e5f65d44c75e599a5378
AvianLearning <iaradcliffe@hotmail.com>
2020-10-16 19:29:02 +0100

    Added functionality to create lists in browser

--- js/app.js
+++ js/app.js
@@ -7,5 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleNewItemFormSubmit = function (event) {
     event.preventDefault();
 
-    
+    const birdListItem = createBirdListItem(event.target);
+    const birdList = document.querySelector('#bird-list');
+    birdList.appendChild(birdListItem);
+
+    event.target.reset();
+}
+
+const createBirdListItem = function (form) {
+    const birdListItem = document.createElement('li');
+    birdListItem.classList.add('bird-list-item');
+
+    const commonName = document.createElement('h2');
+    commonName.textContent = form.common.value;
+    birdListItem.appendChild(commonName);
+
+    const sciName = document.createElement('h3');
+    sciName.textContent = form.sci.value;
+    birdListItem.appendChild(sciName);
+
+    const group = document.createElement('p');
+    group.textContent = form.group.value;
+    birdListItem.appendChild(group);
+
+    return birdListItem;
 }
